1154f6e18812dd19f627ca33f966ad9ffb5088cd
JunnyJun <chbe5082@naver.com>
2023-02-27 01:53:08 +0900

    feat[board] : baseEntity변경

--- src/main/java/com/bomsbro/global/model/entity/BaseTimeEntity.java
+++ src/main/java/com/bomsbro/global/model/entity/BaseTimeEntity.java
@@ -13,21 +13,16 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-/**
- * EntityListeners -> 해당 클래스에 Auditing 기능을 포함
- * AdminApplication 클래스에 @EnableJpaAuditing 어노테이션 적용 후 활성화
- */
 public abstract class BaseTimeEntity { // TODO: default value
     @Column(name = "created_date", updatable = false)
     @CreatedDate
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-    private LocalDateTime createdDate;
+    protected LocalDateTime createdDate;
 
     @Column(name = "modified_date")
     @LastModifiedDate
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-    private LocalDateTime modifiedDate;
+    protected LocalDateTime modifiedDate;
 }

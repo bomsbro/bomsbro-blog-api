8a09803b35b39b486cb49e5de39e4fc49cb31193
JunnyJun <chbe5082@naver.com>
2023-03-02 23:38:09 +0900

    feat[board] : first init usecase

--- src/main/java/com/bomsbro/board/usecase/FirstInit.kt
+++ src/main/java/com/bomsbro/board/usecase/FirstInit.kt
@@ -1,23 +1,21 @@
 package com.bomsbro.board.usecase
 
-import com.bomsbro.board.domain.Board
 import com.bomsbro.board.gateway.external.ExistsGateway
 import com.bomsbro.board.gateway.external.UploadGateway
 import com.bomsbro.board.repository.BoardRepository
 import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
 
-interface CreateBoard {
-    fun create(request: Board): Any
+interface FirstInit {
+    fun firstInit(): Long
 
-    @Component
-    class CreateBoarUseCase(
+    @Service
+    class FirstInitUseCase(
         private val boardRepository: BoardRepository,
         private val existsGateway: ExistsGateway,
         private val uploadGateway: UploadGateway
-    ) : CreateBoard {
-        override fun create(request: Board) {
-            boardRepository.save(request)
-        }
+    ) : FirstInit {
+        override fun firstInit() = boardRepository.firstInit()
     }
 
 }
